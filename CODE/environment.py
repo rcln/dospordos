5216854587b1ffac663267b72899d12785ae036b
JU-2094 <urbgon@gmail.com>
2017-12-03 21:52:46 -0600

    more todo's

--- CODE/environment.py
+++ CODE/environment.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import pandas as pd
 from queue import Queue
 
 
@@ -9,22 +10,29 @@ class Environment:
 
     def __init__(self):
         self.path = "~/dospordos/DATA/train_db/"
+        self.path_db = "~/dospordos/DATA/db_fer/"
         self.queries = {}
         self.current_query = None
+        self.current_data = {}
+        self.data_db = None
 
     def set_path_files(self, path):
         self.path = path
 
+    def set_path_data_db(self, path):
+        self.path_db = path
+
     # start new episode
     def reset(self, id_person):
 
-        initial_state = []
         files = self.path + str(id_person)+"/"
         if not os.path.exists(files):
             raise ValueError('path given doesn\'t exits:\n'+files)
 
         self.queries.clear()
+        self.current_data.clear()
         self.current_query = None
+        self._get_data_db()
         for file in os.listdir(files):
             with open(files+file) as f:
                 data_raw = f.read()
@@ -34,22 +42,24 @@ class Environment:
             for num in data:
                 num_snippet.append(int(num))
             num_snippet = sorted(num_snippet)
-            search = ""
             for i in num_snippet:
-                search = data[str(i)]['search']
                 q.put(data[str(i)])
             if self.current_query is None:
-                self.current_query = search
-            self.queries[search] = q
+                self.current_query = 0
+            self.queries[len(self.queries)] = q
+
+        initial_state = self.get_state()
+
         return initial_state
 
     def step(self, action_query, action_db):
-        reward = 0
-        next_state = []
 
         action_query()
         action_db()
-        done = self.queries[self.current_query].qsize() == 0
+
+        next_state = self.get_state()
+        reward = self._get_reward()
+        done = self._check_grid()
 
         return reward, next_state, done
 
@@ -59,8 +69,56 @@ class Environment:
             queries.append(k)
         return queries
 
+    def get_state(self):
+        state = []
 
+        # Todo get confidence scores
+        # self._get_confidence(state)
 
+        # Todo get tf-idf..?
+
+        # common, Total  (only Univ),   common, Total (only year),  common, Total(U-A)
+        data_db = self.data_db[1]
+        data_cur = self.current_data.get(1, None)
+        if data_cur is None:
+            state.extend([0]*6)
+            return state
+
+        # Todo compare tuples efficiently
+
+        return state
+
+    def _get_data_db(self):
+        # todo do a query or search in fer data..?
+        self.data_db = {0: 'name', 1: [('univ', 'anio')]}
+
+    def _check_grid(self):
+        empty = False
+
+        for k in self.queries.keys():
+            if self.queries[k].qsize() == 0:
+                empty = True
+                break
+        return empty
+
+    def _get_reward(self):
+        reward = self._get_jaccard_distance()
+
+        # todo add penalties due to size of data
+
+        return reward
+
+    def _get_jaccard_distance(self):
+        # dj(A,B) = 1 - J(A,B)
+        # J(A,B) = |A^B| / |AvB|
+        # Todo take consideration of semantic similarities
+        # Todo duda intersección y union de tuplas
+        dist = 0
+        return dist
+
+    @staticmethod
+    def _get_confidence(state): # Todo Theo's working on this
+        state.append(1)
 
 
 
