434cd36db552bdfb34e19abffb63813aafc69761
JU-2094 <urbgon@gmail.com>
2018-02-16 15:17:12 -0600

    selection of max argument for the next actions

--- CODE/DQN_implementation.py
+++ CODE/DQN_implementation.py
@@ -10,7 +10,7 @@ def main():
 
     data_path = "".join(s+"/" for s in (os.getcwd().split('/')[:-1])) + "DATA/"
     data_train = data_path+"train_db/"
-    data_db = data_path+"fer_db/train.json"
+    data_db = data_path+"db_fer/train.json"
 
     # loading users
     list_users = sorted(list(map(int, os.listdir(data_train))))
@@ -21,7 +21,7 @@ def main():
     env.set_path_train(data_train)
     env.set_path_files(data_db)
 
-    agent.print_model()
+    # agent.print_model()
 
     # episodes
     for us in list_users:
@@ -37,21 +37,33 @@ def main():
             """
             Select an action with an epsilon 
             """
-            action_vector = None
+
             p = np.random.random()
             if p < eps:
                 # TODO make a real random vector theo
-                action_vector = [1,0,0,0,0,0]
+                action_vector = [1, 0, 0, 0, 0, 0]
             else:
                 # a = argmaxQ(s,a)
+                arg_max = []
+                for i in range(6):
+                    action_vector = [0]*6
+                    action_vector[i] = 1
+                    in_vector = [state + action_vector]
+                    in_vector = np.array(in_vector)
+                    arg_max.append(agent.network.predict(in_vector))
+
+                action_vector = [0]*6
+                action_vector[arg_max.index(max(arg_max))] = 1
+
                 pass
 
             # Observe reward and new state
             # example
-            reward, nextx_state, done = env.step(agent.next_snippet, agent.change_db)
+            # reward, next_state, done = env.step(agent.next_snippet, agent.)
 
             # Q[s,a] = Q[s,a] + learning_rate*(reward + discount* max_a'(Q[s',a']) - Q[s,a])
             # state = next_state
+            done = True
         break
 
 if __name__ == "__main__":
