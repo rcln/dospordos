d6fc9283e976570fc39f40e3b032a30ca5f739ed
tuxedo21 <ctps.21@comunidad.unam.mx>
2018-03-10 18:58:22 -0600

    added preprocessing

---
+++ CODE/preprocessing.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import pickle
+
+
+def snippets_to_list():
+    folders = os.listdir(os.getcwd() + "/../DATA/train_db/")
+    mega_list = []
+    for folder in folders:
+        folder_content = os.listdir(os.getcwd() + "/../DATA/train_db/" + folder)
+        for json_query in folder_content:
+            json_file = open(os.getcwd() + "/../DATA/train_db/" + folder + "/" + json_query)
+            json_str  = json_file.read()
+            json_data = json.loads(json_str)
+            json_keys = json_data.keys()
+            for json_key in json_keys:
+                mega_list.append(json_data[json_key]["text"] + " " + json_data[json_key]["text"])
+    return mega_list
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+
+def list_to_pickle_count_vectorizer():
+    tf_vectorizer = CountVectorizer(stop_words='english')
+    tf = tf_vectorizer.fit(snippets_to_list())
+    save_object(tf, 'count_vectorizer.pkl')
+
+
+def list_to_pickle_tfidf_vectorizer():
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    tf = tfidf_vectorizer.fit(snippets_to_list())
+    save_object(tf, 'tfidf_vectorizer.pkl')
+
+if __name__ == "__main__":
+    # List all the folders of train_db
+    print(len(snippets_to_list()))
+    list_to_pickle_count_vectorizer()
+    list_to_pickle_tfidf_vectorizer()
+
